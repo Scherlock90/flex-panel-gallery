15307ab757788d602213a5cfb87680c156eb9dd5
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-04-15 16:58:02 +0200

    adding new file

--- src/FlexComponents/Flex-panel-gallery.js
+++ src/FlexComponents/Flex-panel-gallery.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import './Flex-gallery.css';
-import sen from './Images/sen.jpg';
-import puma from './Images/puma.jpg';
+import branicki from './Images/branicki.jpg';
+import roadTorun from './Images/roadTorun.jpg';
+import sliwnoKladki from './Images/sliwnoKladki.jpg';
+import sopotMolo from './Images/sopotMolo.jpg';
 
 const imgStyle = {
   width: '280px',
   height: '400px'
 }
 
+const dataImage = [
+  {
+    img: 'branicki.jpg'
+  },
+  {
+    img: 'roadTorun.jpg'
+  },
+  {
+    img: 'sliwnoKladki.jpg'
+  },
+  {
+    img: 'sopotMolo.jpg'
+  }
+]
 
 export default function FlexPanelGallery () {
   return (
     <div className="container">
       <div className="box">
-          <img style={imgStyle} src={sen} />
+          <img style={imgStyle} src={sliwnoKladki} />
       </div>
       <div className="box">
-          <img  style={imgStyle}src={puma} />
+          <img  style={imgStyle}src={branicki} />
       </div>
       <div className="box">
-          <img  style={imgStyle}src={sen} />
+          <img  style={imgStyle}src={roadTorun} />
       </div>
       <div className="box">
-          <img style={imgStyle} src={puma} />
+          <img style={imgStyle} src={sopotMolo} />
       </div>
     </div>
   )
