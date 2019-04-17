ac926af6fac2e416559bc0325651138984f16df1
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-04-17 23:56:33 +0200

    Updates

---
+++ precache-manifest.1b4270114c0c5e1bdc18de292659df32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4e38e116397acca9abf",
+    "url": "/flex-panel-gallery/static/js/runtime~main.b3676a7d.js"
+  },
+  {
+    "revision": "eaafe20d8509be706a63",
+    "url": "/flex-panel-gallery/static/js/main.27d43089.chunk.js"
+  },
+  {
+    "revision": "1c082208d9d64c3778e5",
+    "url": "/flex-panel-gallery/static/js/2.06f7bf13.chunk.js"
+  },
+  {
+    "revision": "eaafe20d8509be706a63",
+    "url": "/flex-panel-gallery/static/css/main.06835e43.chunk.css"
+  },
+  {
+    "revision": "9a5dec33a7002abd2d65a6f3e63da806",
+    "url": "/flex-panel-gallery/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/flex-panel-gallery/precache-manifest.732f62aeda49f87140eb078ad93bbdbf.js"
+  "/flex-panel-gallery/precache-manifest.1b4270114c0c5e1bdc18de292659df32.js"
 );
 
 workbox.clientsClaim();
